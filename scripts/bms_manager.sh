244d85916c18bd4e226eb8907defa093545f81ac
Sahand <s.hosseini@stud.uni-hannover.de>
2023-12-18 17:35:22 +0000

    changes to start on startup

--- scripts/bms_manager.sh
+++ scripts/bms_manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 
+sudo bash -c "export ROS_MASTER_URI=http://roscore:11311 &&  source /opt/ros/noetic/setup.bash && source /home/ubuntu/bms_manager_ws/devel/setup.bash && python3 /home/ubuntu/bms_manager_ws/src/bms_manager/scripts/bms_manager_node.py"
 
-sudo bash -c "export ROS_MASTER_URI=$ROS_MASTER_URI &&  source /opt/ros/noetic/setup.bash && source /home/ubuntu/bms_manager_ws/devel/setup.bash && rosrun bms_manager bms_manager_node.py "
+#sudo bash -c "export ROS_MASTER_URI=$ROS_MASTER_URI &&  source /opt/ros/noetic/setup.bash && source /home/ubuntu/bms_manager_ws/devel/setup.bash && rosrun bms_manager bms_manager_node.py "
--- test/bms_led2.py
+++ test/bms_led2.py
@@ -9,7 +9,7 @@ def update(strip, strip_part_On):
             if i <= strip_part_On:
                 shade_g = int(255*strip_part_On/12)
                 shade_r = int(-255*strip_part_On/12 + 255)
-                strip[-i-1] = (0,shade_r,shade_g) #BRG
+                strip[-i-1] = (0,0.8*shade_r+20,shade_g*0.8+20) #BRG
                 #self.strip.setPixelColor(i, Color(255,0,0))
             else:
                 strip[-i-1] = (0,0,0)
@@ -23,10 +23,10 @@ if __name__ == '__main__':
     while True: 
         try: 
             if (BMS_Percentage > 100): BMS_Percentage = 0
-            update(strip, int( BMS_Percentage / 100 * 12) )
+            update(strip, int( BMS_Percentage / 100 * 12))
             print(BMS_Percentage / 100 * 12,  int( BMS_Percentage / 100 * 12) )
             BMS_Percentage += 5
-            time.sleep(1)
+            time.sleep(0.5)
         except KeyboardInterrupt:
             strip.fill((0,0,0))
             break
