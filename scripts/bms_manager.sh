528132b66c7fe732686c01041a766eaf653b44f1
pumablattlaus <78273962+pumablattlaus@users.noreply.github.com>
2024-01-12 16:00:31 +0100

    Update bms_manager.sh

--- scripts/bms_manager.sh
+++ scripts/bms_manager.sh
@@ -2,6 +2,6 @@
 
 sudo bash -c "export ROS_MASTER_URI=http://localhost:11311 && source /opt/ros/noetic/setup.bash && roscore &"
 
-sudo bash -c "export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages:/home/rosmatch/.local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages' && export ROS_MASTER_URI=http://localhost:11311 && source /opt/ros/noetic/setup.bash && roscore & python3 /home/rosmatch/catkin_ws/src/bms_manager/scripts/bms_manager_node.py"
+sudo bash -c "export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages:/home/rosmatch/.local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages' && export ROS_MASTER_URI=http://localhost:11311 && source /opt/ros/noetic/setup.bash && python3 /home/rosmatch/catkin_ws/src/bms_manager/scripts/bms_manager_node.py"
 
 #sudo bash -c "export ROS_MASTER_URI=$ROS_MASTER_URI &&  source /opt/ros/noetic/setup.bash && source /home/ubuntu/bms_manager_ws/devel/setup.bash && rosrun bms_manager bms_manager_node.py "
